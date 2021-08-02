15994093cb5638f87ec637dd56cf465b2c485394
Favour Samuel <sfchidimma@gmail.com>
2021-08-02 13:19:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Random Choice Picker
+Project #13 in Brad Traversy's 50 Projects in 50 Days. This project is a random choice picker. Various options are inputted, each must be separated by a comma and the enter button is pressed when done. This randomly selects one of the options. 
+
+![Random Choice Picker](screenshot.jpg)
+
+## Built With
+- [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+- [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Install
+```
+    $ git clone git@github.com:samiiexx/sam-js-13.git
+    $ cd main
+```  
+## Contributors
+- Favour Samuel (@samiiexx)
+
+## License
+Copyright (c) 2021 Favour Samuel.
+
+For enquiries please contact me at [sfchidimma@gmail.com](mailto:sfchidimma@gmail.com).
---
+++ index.js
@@ -0,0 +1,80 @@
+// Copyright 2021 sfchi
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const tagsElement = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10);
+
+        randomSelect()
+    }
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsElement.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagElement = document.createElement('span')
+        tagElement.classList.add('tag')
+        tagElement.innerText = tag
+        tagsElement.appendChild(tagElement)
+    })
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
